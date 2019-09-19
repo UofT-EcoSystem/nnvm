1d52ad0f68dc535699416d52054d30c19ca9ac51
ArmageddonKnight <bojian.zheng@mail.utoronto.ca>
2019-09-19 15:24:35 -0400

    Refactoring 'GradientV2'.

--- src/pass/gradient v2.cc
+++ src/pass/gradient v2.cc
@@ -1,15 +1,10 @@
-/*!
- *  Copyright (c) 2016 by Contributors
- * \file gradients.cc
- * \brief Passes that takes gradient of the graph
- * This code code was modified based on mxnet codebase by Min Lin
- */
-#include <nnvm/pass.h>
-#include <nnvm/pass_functions.h>
-#include <nnvm/op_attr_types.h>
-#include <nnvm/graph_attr_types.h>
 #include <algorithm>
 #include <functional>
+
+#include <nnvm/graph_attr_types.h>
+#include <nnvm/pass.h>
+#include <nnvm/op_attr_types.h>
+#include <nnvm/pass_functions.h>
 
 namespace nnvm {
 namespace pass {
@@ -18,20 +13,21 @@ namespace {
 // A guard that logs mirroring information for only once.
 static bool logged_do_mirror = false;
 
-// default aggregate gradient function
-// require operator __zero__ and __ewise_sum__ to be presented.
+/// @brief Default Gradient Aggregation Function
 NodeEntry DefaultAggregateGradient(std::vector<NodeEntry>&& v) {
   if (v.size() == 1) {
     return std::move(v[0]);
   } else if (v.size() == 0) {
-    NodePtr zero_node = Node::Create();
-    zero_node->attrs.op = Op::Get("__zero__");
-    return NodeEntry{zero_node, 0, 0};
+    NodePtr agg_node = Node::Create();
+
+    agg_node->attrs.op = Op::Get("__zero__");
+    return NodeEntry{agg_node, 0, 0};
   } else {
-    NodePtr sum_node = Node::Create();
-    sum_node->attrs.op = Op::Get("__ewise_sum__");
-    sum_node->inputs = std::move(v);
-    return NodeEntry{sum_node, 0, 0};
+    NodePtr agg_node = Node::Create();
+
+    agg_node->attrs.op = Op::Get("__ewise_sum__");
+    agg_node->inputs   = std::move(v);
+    return NodeEntry{agg_node, 0, 0};
   }
 }
 
@@ -51,232 +47,28 @@ bool CheckGradAllZero(const std::vector<NodeEntry>& grads,
   return true;
 }
 
-// helper entry
 struct GradEntry {
-#ifdef _MSC_VER
   NodeEntry sum = NodeEntry{nullptr, 0, 0};
-#else
-  NodeEntry sum{nullptr, 0, 0};
-#endif
   std::vector<NodeEntry> grads;
-  bool need_attr_hint{true};
+  bool need_attr_hint = true;
 };
-
-#if BASELINE_BACKWARD_MIRRORING
-
-Graph Gradient(Graph src) {
-  if (!logged_do_mirror) {
-    if (dmlc::GetEnv("MXNET_BACKWARD_DO_MIRROR", 0)) {
-      LOG(INFO) << "MXNet has the BASELINE "
-                << "mirroring algorithm enabled";
-    } else {
-      LOG(INFO) << "MXNet has backward mirroring disabled";
-    }
-    logged_do_mirror = true;
-  }
-
-  using nnvm::FGradient;
-  using MirrorFun = std::function<bool (const NodePtr&)>;
-  using AttrHintFun = std::function<NodeEntry (const NodeEntry& src, const NodeEntry &like)>;
-
-  CHECK_NE(src.attrs.count("grad_ys"), 0U)
-      << "Gradient require grad_ys to be presented.";
-  CHECK_NE(src.attrs.count("grad_ys_out_grad"), 0U)
-      << "Gradient require grad_ys_out_grad to be presented.";
-  CHECK_NE(src.attrs.count("grad_xs"), 0U)
-      << "Gradient require grad_xs to be presented.";
-  const std::vector<NodeEntry>& ys =
-      src.GetAttr<std::vector<NodeEntry> >("grad_ys");
-  const std::vector<NodeEntry>& ys_out_grad =
-      src.GetAttr<std::vector<NodeEntry> >("grad_ys_out_grad");
-  const std::vector<NodeEntry>& xs =
-      src.GetAttr<std::vector<NodeEntry> >("grad_xs");
-  using AggFun = std::function<NodeEntry (std::vector<NodeEntry>&& inputs)>;
-  AggFun agg_fun = DefaultAggregateGradient;
-  if (src.attrs.count("grad_aggregate_fun") != 0) {
-    agg_fun = src.GetAttr<AggFun>("grad_aggregate_fun");
-  }
-  MirrorFun mirror_fun = nullptr;
-  if (src.attrs.count("grad_mirror_fun") != 0) {
-    mirror_fun = src.GetAttr<MirrorFun>("grad_mirror_fun");
-  }
-  AttrHintFun attr_hint_fun = nullptr;
-  if (src.attrs.count("attr_hint_fun") != 0) {
-    attr_hint_fun = src.GetAttr<AttrHintFun>("attr_hint_fun");
-  }
-  std::vector<const Op*> zero_ops;
-  if (src.attrs.count("zero_ops") != 0) {
-    zero_ops = src.GetAttr<std::vector<const Op*> >("zero_ops");
-  }
-  const Op* copy_op = (src.attrs.count("copy_op") != 0) ?
-      Op::Get(src.GetAttr<std::string>("copy_op")) :
-      nullptr;
-
-  // topo sort
-  std::vector<NodePtr> topo_order;
-  std::unordered_map<Node*, std::vector<GradEntry> > output_grads;
-
-  DFSVisit(ys, [&](const NodePtr& node) {
-      if (output_grads.count(node.get()) == 0) {
-        output_grads[node.get()].resize(node->num_outputs());
-      }
-      topo_order.push_back(node);
-    });
-
-  CHECK_EQ(ys.size(), ys_out_grad.size());
-  for (size_t i = 0; i < ys.size(); ++i) {
-    NodeEntry ograd = ys_out_grad[i];
-    output_grads[ys[i].node.get()][ys[i].index].grads = { ograd };
-  }
-
-  // Check that all xs are reachable from ys
-  for (size_t i = 0; i < xs.size(); ++i) {
-    CHECK(output_grads.find(xs[i].node.get()) != output_grads.end())
-        << "Cannot differentiate with respect to the " << i+1 << "-th variable "
-        << "because it is unreachable from the outputs.";
-  }
-
-  // construct mirror reduece memory strategy if needed
-  std::unordered_map<Node*, NodePtr> mirror_map;
-  if (mirror_fun != nullptr) {
-    for (const NodePtr& n : topo_order) {
-      if (mirror_fun(n)) {
-        NodePtr new_node = Node::Create();
-        *new_node = *n;
-        new_node->attrs.name += "_mirror";
-        for (auto& e : new_node->inputs) {
-          e.node = mirror_map.at(e.node.get());
-        }
-        for (auto& n : new_node->control_deps) {
-          n = mirror_map.at(n.get());
-        }
-        mirror_map[n.get()] = std::move(new_node);
-      } else {
-        mirror_map[n.get()] = n;
-      }
-    }
-  }
-
-  // traverse backward
-  static auto& grad_fun_map = Op::GetAttr<FGradient>("FGradient");
-  static auto& finfer_shape = Op::GetAttr<FInferShape>("FInferShape");
-
-  std::vector<NodeEntry> out_agg_grads;
-  for (auto rit = topo_order.rbegin(); rit != topo_order.rend(); ++rit) {
-    const NodePtr& ptr = *rit;
-    if (ptr->is_variable()) continue;
-    out_agg_grads.clear();
-    auto& out_grad_vec = output_grads.at(ptr.get());
-    for (uint32_t i = 0; i < out_grad_vec.size(); ++i) {
-      GradEntry& e = out_grad_vec[i];
-      e.sum = agg_fun(std::move(e.grads));
-      if (e.need_attr_hint && attr_hint_fun != nullptr) {
-        e.sum = attr_hint_fun(e.sum, NodeEntry{ptr, 0, i});
-      }
-      out_agg_grads.push_back(e.sum);
-    }
-    if ((*rit)->inputs.size() != 0) {
-      NodePtr fwd_node = (mirror_map.size() == 0 ? ptr : mirror_map.at(ptr.get()));
-      std::vector<NodeEntry> input_grads;
-      if (grad_fun_map.count(ptr->op())) {
-        input_grads = grad_fun_map[ptr->op()](fwd_node, out_agg_grads);
-        CHECK_EQ((*rit)->inputs.size(), input_grads.size())
-            << "Gradient function not returning enough gradient";
-      } else if (CheckGradAllZero(out_agg_grads, zero_ops)) {
-        for (size_t i = 0; i < fwd_node->num_inputs(); ++i) {
-          std::ostringstream os;
-          if (1 == fwd_node->num_inputs()) {
-            os << fwd_node->attrs.name << "_backward";
-          } else {
-            os << fwd_node->attrs.name << "_in" << i << "_backward";
-          }
-          auto p = Node::Create();
-          p->attrs.op = zero_ops[0];
-          p->attrs.name = os.str();
-          p->inputs.push_back(fwd_node->inputs[i]);
-          p->control_deps.emplace_back(fwd_node);
-          if (p->op()->attr_parser != nullptr) {
-            p->op()->attr_parser(&(p->attrs));
-          }
-          input_grads.emplace_back(nnvm::NodeEntry{p, 0, 0});
-        }
-      } else {
-        LOG(FATAL) << "Operator " << fwd_node->op()->name << " is non-differentiable "
-                   << "because it didn't register FGradient attribute.";
-      }
-      auto git = input_grads.begin();
-      for (auto it = (*rit)->inputs.begin(); it != (*rit)->inputs.end(); ++it, ++git) {
-        auto& ge = output_grads[it->node.get()][it->index];
-        // if any of the backward op can do shape inference, the hint is not necessary.
-        if (finfer_shape.count(git->node->op())) {
-          ge.need_attr_hint = false;
-        }
-        ge.grads.emplace_back(std::move(*git));
-      }
-    }
-  }
-  // take out the xs' grads
-  Graph ret;
-  ret.outputs.resize(xs.size());
-  NodeEntryMap<std::pair<size_t, size_t> > unique_grads;
-  size_t counter = 0;
-  for (const NodeEntry& e : xs) {
-    GradEntry& entry = output_grads[e.node.get()][e.index];
-    // aggregate sum if there haven't been
-    if (entry.sum.node.get() == nullptr) {
-      entry.sum = agg_fun(std::move(entry.grads));
-      if (entry.need_attr_hint && attr_hint_fun != nullptr) {
-        entry.sum = attr_hint_fun(entry.sum, e);
-      }
-    }
-    if (copy_op != nullptr) {
-      auto kv = unique_grads.find(entry.sum);
-      if (kv == unique_grads.end()) {
-        unique_grads.emplace(std::move(entry.sum), std::make_pair(1, counter));
-      } else {
-        NodePtr copy_node = Node::Create();
-        std::ostringstream os;
-        os << entry.sum.node->attrs.name << "_" << kv->second.first << "_copy";
-        kv->second.first++;
-        copy_node->attrs.op = copy_op;
-        copy_node->attrs.name = os.str();
-        copy_node->inputs.emplace_back(entry.sum);
-        if (copy_node->attrs.op->attr_parser != nullptr) {
-            copy_node->attrs.op->attr_parser(&(copy_node->attrs));
-        }
-        unique_grads.emplace(NodeEntry{std::move(copy_node), 0, 0}, std::make_pair(1, counter));
-      }
-    } else {
-        ret.outputs[counter] = entry.sum;
-    }
-    ++counter;
-  }
-  if (copy_op != nullptr) {
-    for (const auto& kv : unique_grads) {
-      ret.outputs[kv.second.second] = kv.first;
-    }
-  }
-  return ret;
-}
-
-#else  // !BASELINE_BACKWARD_MIRRORING
 
 /// @brief Auxiliary function that builds a backward graph
 ///          based on the provided mirror function.
 Graph _buildBackwardGraph(
     const Graph& src,
     const std::vector<NodeEntry>& xs,
-    const std::vector<NodePtr>& topo_order,
-    std::unordered_map<Node*, 
-      std::vector<GradEntry> > output_grads,
-    const std::unordered_map<NodePtr, 
-        std::pair<NodePtr, 
-                  NodePtr> >& mirror_map);
+    const std::vector<NodePtr>  & topo_order,
+    // Note that the `output_grads` is made deliberately without `&`.
+    // The reason is because it is a COPY of the output gradients.
+          std::unordered_map<NodePtr, std::vector<GradEntry> > output_grads,
+    const std::unordered_map<NodePtr, std::pair<NodePtr, 
+                                                NodePtr> >& mirror_map);
 
-Graph Gradient(Graph src) {
+Graph GradientV2(Graph src) {
   if (!logged_do_mirror) {
-    if (dmlc::GetEnv("MXNET_BACKWARD_DO_MIRROR", 0)) {
-      LOG(INFO) << "MXNet has the self-defined "
+    if (dmlc::GetEnv("MXNET_BACKWARD_DO_MIRROR_V2", 0)) {
+      LOG(INFO) << "MXNet has the SELF-DEFINED "
                 << "mirroring algorithm enabled";
     } else {
       LOG(INFO) << "MXNet has backward mirroring disabled";
@@ -301,8 +93,8 @@ Graph Gradient(Graph src) {
   using MirrorFun = std::function<MirrorType(
       const NodePtr&)>;
   MirrorFun mirror_fun = nullptr;
-  if (src.attrs.count("grad_mirror_fun") != 0) {
-    mirror_fun = src.GetAttr<MirrorFun>("grad_mirror_fun");
+  if (src.attrs.count("mirror_fun") != 0) {
+    mirror_fun = src.GetAttr<MirrorFun>("mirror_fun");
   }
 
   // initialize topological order and output gradients
@@ -322,7 +114,7 @@ Graph Gradient(Graph src) {
     output_grads[ys[i].node.get()][ys[i].index].grads = { ograd };
   }
 
-  // Check that all xs are reachable from ys
+  // check that all xs are reachable from ys
   for (size_t i = 0; i < xs.size(); ++i) {
     CHECK(output_grads.find(xs[i].node.get()) != output_grads.end())
         << "Cannot differentiate with respect to the " << i+1 << "-th variable "
@@ -330,10 +122,10 @@ Graph Gradient(Graph src) {
   }
 
   std::unordered_map<NodePtr,
-      std::pair<NodePtr,
+      std::pair<NodePtr, 
                 NodePtr> > mirror_map;
-  // record the list of mirrored operators,
-  // for debugging and logging purpose
+  // record the list of mirrored operators, used for 
+  //   debugging and logging purposes
   std::unordered_set<std::string> mirror_ops;
   // record the longest mirror path
   std::pair<std::size_t, std::vector<NodePtr> > longest_mirror_path;
@@ -504,11 +296,8 @@ Graph _buildBackwardGraph(
     const Graph& src,
     const std::vector<NodeEntry>& xs,
     const std::vector<NodePtr>& topo_order,
-    std::unordered_map<Node*, 
-      std::vector<GradEntry> > output_grads,
-    const std::unordered_map<NodePtr, 
-        std::pair<NodePtr,
-                  NodePtr> >& mirror_map) {
+          std::unordered_map<NodePtr, std::vector<GradEntry> > output_grads,
+    const std::unordered_map<NodePtr, std::pair<NodePtr, NodePtr> >& mirror_map) {
   using AttrHintFun = std::function<NodeEntry(
       const NodeEntry& src,
       const NodeEntry& like)>;
@@ -705,18 +494,16 @@ Graph _buildBackwardGraph(
   return ret;
 }
 
-#endif  // BASELINE_BACKWARD_MIRRORING
-
-// register pass
-NNVM_REGISTER_PASS(Gradient)
-.describe("Return a gradient graph of src.attrs[\"ys\"] wrt src.attrs[\"xs\"]")
-.set_body(Gradient)
-.set_change_graph(true)
-.depend_graph_attr("shape_inputs")
-.depend_graph_attr("dtype_inputs")
-.depend_graph_attr("grad_ys")
-.depend_graph_attr("grad_xs")
-.depend_graph_attr("grad_ys_out_grad");
+NNVM_REGISTER_PASS(GradientV2)
+    .describe("Return a gradient graph of src.attrs[\"ys\"] "
+                "w.r.t. src.attrs[\"xs\"]")
+    .set_body(GradientV2)
+    .set_change_graph(true)
+    .depend_graph_attr("shape_inputs")
+    .depend_graph_attr("dtype_inputs")
+    .depend_graph_attr("grad_ys")
+    .depend_graph_attr("grad_xs")
+    .depend_graph_attr("grad_ys_out_grad");
 
 }  // namespace
 }  // namespace pass
